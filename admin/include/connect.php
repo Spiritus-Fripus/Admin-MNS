584745ede4fa7d24b5a09c59eca556e794d6f9d8
Loïc DS <desousa.loic8@gmail.com>
2024-05-21 09:56:11 +0200

    plou

--- admin/include/connect.php
+++ admin/include/connect.php
@@ -7,12 +7,3 @@ try {
 } catch (PDOexception $e) {
     die($e->getMessage());
 }
-
-<?php
-$config = require 'varEnv.php';
-
-try {
-    $db = new PDO("mysql:host=" . $config['HOST'] . ";dbname=" . $config['DB_NAME'] . ";charset=utf8", $config['DB_USER'], $config['DB_PASSWORD']);
-} catch (PDOexception $e) {
-    die($e->getMessage());
-}
